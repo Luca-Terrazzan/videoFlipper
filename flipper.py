2968d2f4335da6545f8665ea2f08e426292c8380
Luca Terrazzan <luca.terraz@gmail.com>
2018-02-03 19:32:41 +0100

    Decreased bitrate

--- flipper.py
+++ flipper.py
@@ -1,12 +1,14 @@
-from moviepy.editor import VideoFileClip, ipython_display
+from moviepy.editor import VideoFileClip
+from moviepy.video.fx.mirror_x import mirror_x
 
-video = VideoFileClip('/Users/luca.terrazzan/Documents/workspace/dm-au/videos/test3.mp4')
+video = VideoFileClip('/Users/luca.terrazzan/Documents/workspace/videoFlipper/videos/original/test.mp4')
 
-ipython_display(video)
+video = mirror_x(video)
 
 video.write_videofile("edited.mp4",
                       temp_audiofile="temp-audio.m4a",
                       remove_temp=True,
                       codec="libx264",
-                      audio_codec="aac"
+                      audio_codec="aac",
+                      audio_bitrate='50k'
                      )
