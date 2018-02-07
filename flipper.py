a55acfd756a047807f5cabc49f3e912cb6fa326f
Luca Terrazzan <luca.terrazzan@docebo.com>
2018-02-07 18:33:57 +0100

    Full flow for single video * mirror * extract audio * raise pitch * export video

--- flipper.py
+++ flipper.py
@@ -1,14 +1,33 @@
+# Video
 from moviepy.editor import VideoFileClip
 from moviepy.video.fx.mirror_x import mirror_x
+# Audio
+import librosa
+from librosa.effects import pitch_shift
+# Conversion
+from pydub import AudioSegment
 
-video = VideoFileClip('/Users/luca.terrazzan/Documents/workspace/videoFlipper/videos/original/test.mp4')
+projectFolder = '/Users/luca.terrazzan/Documents/workspace/videoFlipper/'
+videoFolder = 'videos/original/'
+editedAudioFileName = 'audiotrack.wav'
+editedVideoFileName = 'videotrack.mp4'
 
+# Mirror the video
+video = VideoFileClip(projectFolder + videoFolder + 'test.mp4')
 video = mirror_x(video)
 
-video.write_videofile("edited.mp4",
-                      temp_audiofile="temp-audio.m4a",
-                      remove_temp=True,
-                      codec="libx264",
-                      audio_codec="aac",
-                      audio_bitrate='50k'
-                     )
+# Shift the pitch up a semitone
+audio, sr = librosa.load(projectFolder + videoFolder + 'test.mp4')
+pitchTest = pitch_shift(audio, sr, 1)
+librosa.output.write_wav(projectFolder + editedAudioFileName, pitchTest, sr)
+
+# Convert the shifted audio back to .mp3
+mp3 = AudioSegment.from_wav(projectFolder + editedAudioFileName)
+mp3.export(projectFolder + editedAudioFileName + '.mp3', 'mp3')
+
+# Save the output
+video.write_videofile(
+    projectFolder + editedVideoFileName,
+    remove_temp   = True,
+    audio         = projectFolder + editedAudioFileName + '.mp3'
+)
