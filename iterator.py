f41b10933470ab77a44e8e24d49c3a8dfaeee203
Luca Terrazzan <luca.terraz@gmail.com>
2018-02-14 11:38:53 +0100

    moved source to a separate folder

--- iterator.py
+++ iterator.py
@@ -1,5 +1,7 @@
+import sys
+sys.path.append('./src')
+
 import os
-# from flipper import mirror
 from flipper import mirror
 from flipper import saveVideo
 
@@ -7,13 +9,20 @@ root = './videos/original/'
 done = './videos/done/'
 edited = './videos/edited/'
 
-# Iterates over a directory files
+# Iterates videos to convert
 print('Looking for video files...')
 for filename in os.listdir(root):
+    # If it actually is a video
     if filename.endswith('.mp4'):
         print('Video found: ' + filename)
         print('Mirroring...')
+
+        # Mirror the video
         video = mirror(root + '/' + filename)
+
+        # Save the video
         saveVideo(video, edited + '/' + filename)
+
+        # Move the original video to the done videos directory
         os.rename(root + '/' + filename, done + '/' + filename)
 
