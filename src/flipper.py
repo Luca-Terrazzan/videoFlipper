4fd9581ce95a7b6c0b29e8b6fa966464ab6dcec8
Luca-Terrazzan <luca.terraz@gmail.com>
2018-02-18 16:53:32 +0100

    removed old code

--- src/flipper.py
+++ src/flipper.py
@@ -7,33 +7,6 @@ from librosa.effects import pitch_shift
 # Conversion
 from pydub import AudioSegment
 
-
-def start():
-    projectFolder = '/Users/luca.terrazzan/Documents/workspace/videoFlipper/'
-    videoFolder = 'videos/original/'
-    editedAudioFileName = 'audiotrack.wav'
-    editedVideoFileName = 'videotrack.mp4'
-
-    # Mirror the video
-    video = VideoFileClip(projectFolder + videoFolder + 'test.mp4')
-    video = mirror_x(video)
-
-    # Shift the pitch up a semitone
-    audio, sr = librosa.load(projectFolder + videoFolder + 'test.mp4')
-    pitchTest = pitch_shift(audio, sr, 1)
-
-    # Convert the shifted audio back to .mp3
-    librosa.output.write_wav(projectFolder + editedAudioFileName, pitchTest, sr)
-    mp3 = AudioSegment.from_wav(projectFolder + editedAudioFileName)
-    mp3.export(projectFolder + editedAudioFileName + '.mp3', 'mp3')
-
-    # Save the output
-    video.write_videofile(
-        projectFolder + editedVideoFileName,
-        remove_temp = True,
-        audio       = projectFolder + editedAudioFileName + '.mp3'
-    )
-
 def mirror(path):
     return mirror_x(VideoFileClip(path))
 
